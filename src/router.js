b9a09ee0174d1abfbad82872d9cd15566d2ec085
Fay <1360019840@qq.com>
2019-06-15 14:22:00 +0800

    修改了路由

--- src/router.js
+++ src/router.js
@@ -11,7 +11,15 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      children:[
+        {
+          path: 'user'
+        },
+        {
+          
+        }
+      ]
     },
     {
       path: '/about',
@@ -20,6 +28,9 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/login'
     }
   ]
 })
