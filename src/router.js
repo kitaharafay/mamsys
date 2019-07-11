3db1279a4e26aeb312eb614e9ea62553bc87f732
kitaharafay <1360019840@qq.com>
2019-07-12 00:15:57 +0800

    使用@vue/cli重新配置项目

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import User from './views/User.vue'
 
 Vue.use(Router)
 
@@ -12,18 +11,15 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
-      children: [
-        {
-          path: 'user',
-          component: User
-        }
-      ]
+      component: Home
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import( './views/About.vue')
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,144 +1,18 @@
 <template>
-  <el-container style="min-height:100vh;">
-    <el-header class="elx-header">
-      <div style="float:right;">
-        <el-button icon="el-icon-user" class="elx-header-btn">user</el-button>
-      </div>     
-    </el-header>
-    <el-container>
-      <el-aside width="256px">
-        <el-menu style="height:calc(100vh - 80px);">
-          <el-menu-item index="10">
-            <i class="el-icon-s-home"></i>
-            <span slot="title">首页</span>
-          </el-menu-item>
-          <el-submenu index="8">
-            <template slot="title">
-              <i class="el-icon-setting"></i>
-              <span>系统管理</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="8-1">选项1</el-menu-item>
-              <el-menu-item index="8-2">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="9">
-            <template slot="title">
-              <i class="el-icon-data-analysis"></i>
-              <span>流程设计</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="9-1">选项1</el-menu-item>
-              <el-menu-item index="9-2">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-office-building"></i>
-              <span>质量工程</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-sunrise-1"></i>
-              <span>环保管理</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-              <el-menu-item index="2-3">选项2</el-menu-item>
-              <el-menu-item index="2-4">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-umbrella"></i>
-              <span>安全管理</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
-              <el-menu-item index="3-3">选项2</el-menu-item>
-              <el-menu-item index="3-4">选项2</el-menu-item>
-              <el-menu-item index="3-5">选项2</el-menu-item>
-              <el-menu-item index="3-6">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-collection"></i>
-              <span>综合协调</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="4-1">选项1</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-receiving"></i>
-              <span>竣工资料</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="5-1">选项1</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="6">
-            <template slot="title">
-              <i class="el-icon-notebook-1"></i>
-              <span>实名制管理</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="6-1">选项1</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="7">
-            <template slot="title">
-              <i class="el-icon-set-up"></i>
-              <span>工程建设</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="7-1">选项1</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
-      <el-container>
-        <el-main style="background: rgba(0,0,0,0.1)">
-          <router-view></router-view>
-        </el-main>
-        <el-footer style="background: rgba(0,0,0,0.1)">
-          <p style="font-size:14px;text-align:center">Design by ghaoyang 2019-06-17</p>
-        </el-footer>
-      </el-container>
-    </el-container>
-  </el-container>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
-<style scoped>
-.elx-header {
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.4);
-  z-index: 999;
-  line-height: 60px;
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
 }
-
-.elx-header-btn {
-  border: none;
-  height: 60px;
-  vertical-align: top;
-}
-</style>
-
-
+</script>
