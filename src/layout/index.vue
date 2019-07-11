88534792b99ab0bdf7586c9180c030544a55d131
kitaharafay <1360019840@qq.com>
2019-07-12 01:26:18 +0800

    添加element-ui，使用基本布局

---
+++ src/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="gh-main-warpper">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside>Aside</el-aside>
+      <el-container>
+        <el-main style="overflow:hidden">Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.gh-main-warpper {
+    min-height: 100vh;
+}
+</style>
