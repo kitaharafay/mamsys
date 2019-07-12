2371368aa6951ba8a1be33a20dc1a725434cf7a0
kitaharafay <1360019840@qq.com>
2019-07-13 07:58:39 +0800

    加入左侧导航栏，但是现在不知道如何和路由权限控制结合起来

--- src/layout/components/SideBar.vue
+++ src/layout/components/SideBar.vue
@@ -1,5 +1,30 @@
 <template>
-    <el-aside>
-        
-    </el-aside>
+  <el-aside class="gh-sidebar">
+    <el-menu class="gh-navbar">
+      <el-menu-item index="1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>系统管理</span>
+        </template>
+        <el-menu-item index="2-1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">流程设计</span>
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
 </template>
+
+<style>
+.gh-sidebar {
+    height: calc(100vh - 60px);
+}
+
+.gh-navbar {
+    height: 100%;
+}
+</style>
